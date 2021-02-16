c6fc2832eb793063417c43cc8b1831bf9d9f20a9
gautamb97 <www.gautam971997@gmail.com>
2021-02-16 15:58:47 +0530

    Added:New contact to address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,15 +1,18 @@
 import java.util.*;
+
 public class AddressBookMain {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
-		Person personDetails = new Person();
-		personDetails.creatingContact();
+		Contact personDetails = new Contact("firstName", "last name", "address ", "city ", "state", "email address", 0,
+				0);
+		personDetails.addingContacts();
+
 	}
 
 }
 
-class Person{
+class Contact {
 	String firstName;
 	String lastName;
 	String address;
@@ -18,57 +21,85 @@ class Person{
 	String emailAddress;
 	int zipCode;
 	long phoneNumber;
-	public String getFirstName() {
-		return firstName;
+
+	public Contact(String firstName, String lastName, String address, String city, String state, String emailAddress,
+			int zipCode, long phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.emailAddress = emailAddress;
+		this.zipCode = zipCode;
+		this.phoneNumber = phoneNumber;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	public String getLastName() {
-		return lastName;
-	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public String getAddress() {
-		return address;
-	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public String getCity() {
-		return city;
-	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
-	public String getState() {
-		return state;
-	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
-	public String getEmailAddress() {
-		return emailAddress;
-	}
+
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
-	public int getZipCode() {
-		return zipCode;
-	}
+
 	public void setZipCode(int zipCode) {
 		this.zipCode = zipCode;
 	}
-	public long getPhoneNumber() {
-		return phoneNumber;
-	}
+
 	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-	
-	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
 	static Scanner phoneBookDetails = new Scanner(System.in);
+
 	public void creatingContact() {
 		System.out.println("Enter your first name : ");
 		setFirstName(phoneBookDetails.next());
@@ -87,7 +118,35 @@ class Person{
 		setPhoneNumber(phoneBookDetails.nextLong());
 		System.out.println("Enter your email address : ");
 		setEmailAddress(phoneBookDetails.next());
-		
-		System.out.print("Name : "+getFirstName()+" "+getLastName()+", Address: " + getAddress() + ", City: "+city+", State: "+getState()+", Zip Code: "+getZipCode()+", Phone Number:"+getPhoneNumber()+", Email Address: "+getEmailAddress());
+
 	}
+
+	public void addingContacts() {
+
+		creatingContact();
+		Contact details = new Contact(firstName, lastName, address, city, state, emailAddress, zipCode, phoneNumber);
+		List<Object> personDetailsSet = new ArrayList<>();
+		details.getFirstName();
+		details.getLastName();
+		details.getAddress();
+		details.getCity();
+		details.getState();
+		details.getEmailAddress();
+		details.getZipCode();
+		details.getPhoneNumber();
+		personDetailsSet.add(details);
+
+		System.out.println(
+				"--------------------------------------------------------------------------------------------------");
+		System.out.println(personDetailsSet);
+		System.out.println(
+				"--------------------------------------------------------------------------------------------------");
+	}
+
+	@Override
+	public String toString() {
+		return "[firstName =" + firstName + ", lastName =" + lastName + ", city =" + city + ", state =" + state
+				+ ", zip =" + zipCode + ", phone =" + phoneNumber + ", email address =" + emailAddress + "]\n";
+	}
+
 }
